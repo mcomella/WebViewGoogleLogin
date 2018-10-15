fd953bbc7b352e3226a3ce588988cf4bdca5f2fb
Michael Comella <michael.l.comella@gmail.com>
2018-10-15 16:16:22 -0700

    Add multi-window support.

--- app/src/main/java/xyz/mcomella/webviewgooglelogin/MainActivity.kt
+++ app/src/main/java/xyz/mcomella/webviewgooglelogin/MainActivity.kt
@@ -1,12 +1,16 @@
 package xyz.mcomella.webviewgooglelogin
 
 import android.os.Bundle
+import android.os.Message
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_DPAD_DOWN
 import android.view.KeyEvent.KEYCODE_DPAD_LEFT
 import android.view.KeyEvent.KEYCODE_DPAD_UP
+import android.view.View
+import android.view.View.VISIBLE
 import android.webkit.CookieManager
 import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,6 +21,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initWebView(webView)
+        initWebView(windowWebView)
+
+        CookieManager.getInstance().apply {
+            setAcceptCookie(true)
+            setAcceptThirdPartyCookies(webView, true)
+        }
+
+        // Load other urls with key events (on the emulator) or by swapping this url with others.
+        webView.loadUrl("https://pinterest.com/")
+    }
+
+    private fun initWebView(webView: WebView) {
         webView.settings.apply {
             javaScriptEnabled = true
 
@@ -31,8 +48,8 @@ class MainActivity : AppCompatActivity() {
 
             // We don't support multi-window and google login on nytimes works without it. though desktop and Chrome use multi-window in this flow.
             // Setting up multi-window support doesn't seem to fix this issue either though.
-            setSupportMultipleWindows(false)
-            javaScriptCanOpenWindowsAutomatically = false
+            setSupportMultipleWindows(true)
+            javaScriptCanOpenWindowsAutomatically = true
 
             // Enable everything that seems like it could break the google log in experience.
             databaseEnabled = true
@@ -42,16 +59,8 @@ class MainActivity : AppCompatActivity() {
             loadWithOverviewMode = true
         }
 
-        CookieManager.getInstance().apply {
-            setAcceptCookie(true)
-            setAcceptThirdPartyCookies(webView, true)
-        }
-
-        webView.webChromeClient = WebChromeClient()
+        webView.webChromeClient = MainChromeClient()
         webView.webViewClient = WebViewClient()
-
-        // Load other urls with key events (on the emulator) or by swapping this url with others.
-        webView.loadUrl("https://pinterest.com/")
     }
 
     override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
@@ -64,6 +73,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         return true
+    }
+
+    inner class MainChromeClient : WebChromeClient() {
+        override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+            val windowWebView = WebView(view?.context)
+            initWebView(windowWebView)
+            container.addView(windowWebView)
+            with (resultMsg!!) {
+                (obj as WebView.WebViewTransport).webView = windowWebView
+                sendToTarget()
+            }
+            return true
+        }
+
+        override fun onCloseWindow(window: WebView?) {
+            container.removeView(window)
+        }
     }
 }
 
